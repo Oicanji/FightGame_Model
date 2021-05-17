487dbcc1245c3dc8cb9dd253b081e64e24643fb8
Oicanji <51388779+Oicanji@users.noreply.github.com>
2021-05-17 14:36:44 -0300

    Animator Add

---
+++ Assets/Scripts/Player/Player1/walk_single.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk_single : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    void Update()
+    {
+        //AXIS X 1-
+        if (Input.GetKeyDown(KeyCode.A)){
+            anim.SetInteger("axis x",-1);
+        }
+
+        //AXIS X 1+
+        if (Input.GetKeyDown(KeyCode.D)){
+            anim.SetInteger("axis x",1);
+        }
+
+    }
+}
