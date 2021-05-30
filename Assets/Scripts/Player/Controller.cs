fabfbb8c4e5bb677a95c448d7c04d961f983ec2a
leandro <50909151+leandl@users.noreply.github.com>
2021-05-30 13:42:19 -0300

    + Movimento para frente e para trás
    + Adicionado o método isWalkBack e isWalkFront na class Controller, para verificar se o jogador pressionou os botões para frente e para trás.

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Controller : MonoBehaviour{  
     private ControllerConfig config;
 
     public Controller(ControllerConfig new_config){
         config = new_config;
     }
+
     public bool isWalkLeft(){
         return Input.GetKey(config.analog_left) && !Input.GetKey(config.analog_right);
     }
+
     public bool isWalkRight(){
         return Input.GetKey(config.analog_right) && !Input.GetKey(config.analog_left);
     }
+
+    public bool isWalkBack(bool playerInLeft){
+        return playerInLeft 
+        ? isWalkLeft()
+        : isWalkRight();
+    }
+
+    public bool isWalkFront(bool playerInLeft){
+        return playerInLeft 
+        ? isWalkRight()
+        : isWalkLeft();
+    }
+
     public bool isJump(){
         return Input.GetKeyDown(config.jump);
     }
