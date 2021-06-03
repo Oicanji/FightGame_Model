254be5766571ff12c4305e6fb0657ee7a3a93cf5
Oicanji <51388779+Oicanji@users.noreply.github.com>
2021-06-02 23:21:19 -0300

    att fix

--- Assets/Scripts/Player/Move_Behavior.cs
+++ Assets/Scripts/Player/Move_Behavior.cs
@@ -21,9 +21,12 @@ public class Move_Behavior : MonoBehaviour
     public float speed;
     public float tenacity;
     public float jump_force;
+    public float jump_double_force;
 
     //public status
     public bool OnFloor = false;
+    public int haveDoubleJump = 0;
+    //private status
     
     public SpriteRenderer sprite;
     void Start(){
@@ -40,6 +43,7 @@ public class Move_Behavior : MonoBehaviour
 
         bool isWalk = ( x != 0 );
         anim.SetBool("walk", isWalk);
+        anim.SetFloat("axis x", x);
         
         float moveBy = (slow)
         ? (x * tenacity)
@@ -48,11 +52,22 @@ public class Move_Behavior : MonoBehaviour
         rb.velocity = new Vector2(moveBy, rb.velocity.y); 
     }
     public void Jump(){
-        if(!OnFloor) return;
-
-        rb.AddForce(new Vector2(jump_force, jump_force), ForceMode2D.Impulse);
-        OnFloor = false;
-        anim.SetBool("jump", true);
+        if(OnFloor){
+            rb.AddForce(new Vector2(jump_force, jump_force), ForceMode2D.Impulse);
+            OnFloor = false;
+            anim.SetBool("jump", true);
+            haveDoubleJump++;
+        }else if(haveDoubleJump>0){
+            if(rb.velocity[1] > 0.0f){
+                rb.AddForce(new Vector2(jump_double_force/1.5f, jump_double_force/1.5f), ForceMode2D.Impulse);
+            }else if(rb.velocity[1] < 0.0f && rb.velocity[1] > -3.0f){
+                rb.AddForce(new Vector2(jump_double_force, jump_double_force), ForceMode2D.Impulse);
+            }else{
+                rb.AddForce(new Vector2(jump_double_force*1.5f, jump_double_force*1.5f), ForceMode2D.Impulse);
+            }
+            anim.SetBool("double_jump", true);
+            haveDoubleJump--;
+        }
     }
     public void Squat(){
         if(OnFloor){
@@ -64,8 +79,10 @@ public class Move_Behavior : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag != "Floor") return;
-            
+        
+        haveDoubleJump = 0;
         anim.SetBool("jump",false);
+        anim.SetBool("double_jump", false);
         OnFloor = true;
     }
 }
